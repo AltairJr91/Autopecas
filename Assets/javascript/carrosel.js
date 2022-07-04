20c05a339ce83765837408a24b4288cf8d29e11b
Altair <mbob_metallica@hotmail.com>
2022-07-03 22:58:00 -0300

    beta 0.8

---
+++ Assets/javascript/carrosel.js
@@ -0,0 +1,15 @@
+
+const imagens = document.getElementById("img");
+const img = document.querySelectorAll(".img__fit");
+
+var imgx = 0;
+
+function carrosel(){
+    imgx++
+    if(imgx > img.length -1){
+        imgx =0;
+    }
+    imagens.style.transform = `translateX(${-imgx * 200}px)`;
+}
+
+setInterval(carrosel, 1800);
